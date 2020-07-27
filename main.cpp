c8e87e5c3cf7ff38a0d25100ff89c679f49a27a4
eunsagn <dmstkdrmatkd@naver.com>
2020-07-27 11:41:03 +0000

    first

--- main.cpp
+++ main.cpp
@@ -1,47 +1,6 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-  
-  int th=3, fi=5, fit=15;
-  int t =0;
-  int sum=0;
-
-  printf("%d\n", sum);
-/*
-  for(th=3; th<1000; th += 3){
-    sum += th;
-  }
-
-  for(fi=5; fi<1000; th += 5){
-    sum += fi;
-  }
-
-  for(fit=15; fit<1000; fit += 15){
-    sum -= fit;
-  }
-*/
-
-  for(t=1; t<1000; t++){
-    if(t % 3 ==0 || t % 5 == 0)
-    sum += t;
-  }
-/*
-  while(1){
-    sum += th;
-    sum += fi;
-    sum -= fit;
-    
-    th += 3;
-    fi += 5;
-    fit += 15;
-    
-    if(th > 1000)
-    if(fi > 1000)
-    if(fit > 1000)
-  }
-
-*/
-
-  printf("%d\n", sum);
-
+int main()
+{
+	return 0;
 }
